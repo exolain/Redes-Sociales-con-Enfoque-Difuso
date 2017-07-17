1aef2bb7298da7118398ace7a3978208639aefb0
exolain <nmarinpe@gmail.com>
2017-07-16 23:42:21 -0600

    Updating generate_json

--- preparacion_datos_para_visualizacion/generate_json.py
+++ preparacion_datos_para_visualizacion/generate_json.py
@@ -8,17 +8,25 @@ initial_year=2014
 final_year=2016
 for year in range(initial_year,final_year+1):
     input_membership="/home/lain/Redes-Sociales-con-Enfoque-Difuso/membership"+str(year)+".csv"
-    input_data="../data/clean/"+str(year)+"_v3_migrantes.csv"
-    output_data_to_json="../data/clean/to_json/"+str(year)+"_v3_migrantes_to_process_to_json.csv"
-    output_data_final="../data/clean/to_json/"+str(year)+"_v3_migrantes_to_process.csv"
-    links_to_json="../data/clean/to_json/"+str(year)+"_links_to_json.json"
+    input_data="../data/clean/"+str(year)+"_v4_migrantes.csv"
+    output_data_to_json="../data/clean/to_json2/"+str(year)+"_v4_migrantes_to_process_to_json.csv"
+    output_data_final="../data/clean/to_json2/"+str(year)+"_v4_migrantes_to_process.csv"
+    links_to_json="../data/clean/to_json2/"+str(year)+"_links_to_json.json"
     region_names=["Region Central", "Region Chorotega", "Region Pacifico Central", "Region Brunca", "Region Huetar Caribe", "Region Huetar Norte"]
     count_year=0
     last_index=0
-    df_data = pd.read_csv(input_data,index_col=False, header=0, usecols=['region_residencia', 'residencia_hace_dos_anos'])
+    mf_dic = {'1.0': 'Mantiene familia con salario minimo', '0.75': 'Mantiene familia con salario medio bajo', '0.3': 'Mantiene familia con salario medio', '0.0':'No mantiene familia'}
+    titulo_dic = {'1.0': 'No tiene educacion', '0.9': 'Educacion primaria', '0.7': 'Educacion Secundaria', '0.0': 'Educacion superior'}
+    inp_dic = {'1.0': 'Salario bajo', '0.8': 'Salario medio bajo', '0.2': 'Salario medio', '0.0': 'Salario alto'}
+    cl_dic = {'1.0': 'Desempleado', '0.0': 'Ocupado', '0.7': 'Retirado'}
+
+    df_data = pd.read_csv(input_data,index_col=False, header=0, usecols=['region_residencia', 'residencia_hace_dos_anos', 
+                                                                          'condicion_laboral', 'ingreso_por_persona',
+                                                                          'titulo', 'mantiene_familia'])
     links = pd.DataFrame(df_data)
 
-    df = pd.crosstab(links['residencia_hace_dos_anos'], links['region_residencia'])
+    #df = pd.crosstab(links['residencia_hace_dos_anos'], links['region_residencia'])
+    df = pd.crosstab(links['region_residencia'], links['residencia_hace_dos_anos'])
     print df
     link_array = []
     for  column in df:
@@ -29,7 +37,7 @@ for year in range(initial_year,final_year+1):
                     last_index = index-1
 
 
-    df_memb = pd.read_csv(input_membership,index_col=False, header=0, usecols=["V1","V2","V3"] )
+    df_memb = pd.read_csv(input_membership,index_col=False, header=0, usecols=["V1","V2","V3","V4"] )
     df_output = pd.concat([df_data, df_memb], axis=1)
     df_output.to_csv(output_data_final)
     columns = []
@@ -59,7 +67,22 @@ for year in range(initial_year,final_year+1):
                 new_column="nodes.Y"+str(year)+"."+str(uniq_index)+".membership."+str(cluster_number)+".percent"
                 columns.append(new_column)
             
-            
+            if "mantiene_familia" in column:
+                new_column = "nodes.Y"+str(year)+"."+str(uniq_index)+'.mantieneFamilia'
+                columns.append(new_column)
+
+            if "titulo" in column:
+                new_column = "nodes.Y"+str(year)+"."+str(uniq_index)+'.titulo'
+                columns.append(new_column)
+
+            if "condicion_laboral" in column:
+                new_column = "nodes.Y"+str(year)+"."+str(uniq_index)+'.condicionLaboral'
+                columns.append(new_column)
+
+            if "ingreso_por_persona" in column:
+                new_column = "nodes.Y"+str(year)+"."+str(uniq_index)+'.ingresoPorPersona'
+                columns.append(new_column)
+
             uniq_index=uniq_index+1
 
     with open(output_data_to_json, 'wb') as migration_graph:
@@ -88,6 +111,17 @@ for year in range(initial_year,final_year+1):
             if "hace_dos" in column and  not row.empty:
                 rows.append(row[column])
 
+            if "mantiene_familia" in column and  not row.empty:
+                rows.append(mf_dic[str(row[column])])
+
+            if "titulo" in column and  not row.empty:
+                rows.append(titulo_dic[str(row[column])])
+
+            if "condicion_laboral" in column and  not row.empty:
+                rows.append(cl_dic[str(row[column])])
+
+            if "ingreso_por_persona" in column and  not row.empty:
+                rows.append(inp_dic[str(row[column])])
 
     with open(links_to_json, 'w') as outfile:
         json.dump(link_array, outfile)
